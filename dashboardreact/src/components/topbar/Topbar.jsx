f7bcd785dad0855a2ebd95b2073c1f77c291d148
fedegrimo <fede.grimo@gmail.com>
2022-03-01 16:12:03 -0300

    [UPDATE] Logo y foto Perfil

--- dashboardreact/src/components/topbar/Topbar.jsx
+++ dashboardreact/src/components/topbar/Topbar.jsx
@@ -6,14 +6,14 @@ export default function Topbar() {
     <div className="topbar">
       <div className="topbarWrapper">
         <div className="topLeft">
-          <span className="logo">EcoShop</span>
+          <img src="../images/logo.png" />
         </div>
         <div className="topRight">
           
           <div className="topbarIconContainer">
            <ExitToAppIcon/>
           </div>
-          <img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="topAvatar" />
+          <img src="../images/profile.png?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="topAvatar" />
         </div>
       </div>
     </div>
